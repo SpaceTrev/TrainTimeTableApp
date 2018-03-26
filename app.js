5b0f9949a2dffaa9d526d128491edece6c50684f
Trevor Benavides <trevorbenavides@Trevors-MacBook-Pro.local>
2018-03-26 01:05:33 -0600

    progress

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-var trains = {
-    trainName: '',
-    destination: "12:00pm",
-    firstTrainTime: '03-18-2018T12:00:00.555',
-    frequency: 120,
-};
+var trains = [
+    {
+        trainName: 'The Juxtapose Express',
+        destination: "Prague, Czech Republic",
+        firstTrainTime: '05:00',
+        frequency: 45,
+    },
+    {
+        trainName: "Mid-Day Belgium Shuffle",
+        destination: "Brussels, Beligum",
+        firstTrainTime: '12:00',
+        frequency: 120
+    },
+    {
+        trainName: "French Wine and Dine Montage",
+        destination: "Paris, France",
+        firstTrainTime: "15:00",
+        frequency: 120
+    }
+];
 
 var config = {
     apiKey: "AIzaSyCkub7V9GVdtqrVHHfzJwALAXJxf242lWk",
@@ -15,13 +29,35 @@ var config = {
 };
 firebase.initializeApp(config);
 var database = firebase.database();
-function writeUserData(trainObj) {
+function writeUserData(trains) {
     console.log("write user data");
-    database.ref("traintimes/").push({
-        trains: trains, 
+    database.ref("trains/").set({
+        schedule_Data_Object: trains
+    })
+    console.log(trains);
+}
+writeUserData(trains);
+function displayNewTrain(name) {
+    var trainName = $("#formControlInput0").val().trim();
+    var trainDestination = $("#formControlInput1").val();
+    var firstDepartTime = $("#formControlSelect0").val();
+    var frequency = $("#formControlSelect1").val();
+    var newTrainObject = {
+        trainName: trainName,
+        destination: trainDestination,
+        firstTrainTime: firstDepartTime,
+        frequency: frequency
+    }
+    trains.push(newTrainObject);
+    database.ref("trains/").push({
+        newTrainObject: newTrainObject
     })
 }
-writeUserData(trainTimes);
+$(document).on('click', '#addTrainButton', displayNewTrain);
+// $(document).on(ready, function(){
+
+// })
+
 
 
 
